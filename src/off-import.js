0e97ed84ae26707f6e0f43828d601cc28fe7631b
slim <smedini@gmail.com>
2018-10-07 19:58:40 +0200

    off-import 99,999%

--- src/off-import.js
+++ src/off-import.js
@@ -23,17 +23,18 @@ export const offImport = async ([offDb, bfDb]) => {
     let offCount = 0
     let trunkCount = 0
     let trunkWithFacetCount = 0
-    let facetCount = 0
+    let writenFacets = 0
     let trunkBuffer = []
     let facetBuffer = []
     let trunkFacets = 0
+    let bufferSize = 100
 
-    offCol.find({}, offFields).limit(200)
+    //aidx => par paquet de 1000, puis bulkWrite les deux
+    offCol.find({}, offFields).limit(250)
         .forEach(async off => {
-            debug(offCount)
             offCount++
-            if (offCount % 2000 === 0) {
-                debug("counts: off=%o trunk=%o trunkWFacets=%o facetStored=%o", offCount, trunkCount, trunkWithFacetCount, facetCount)
+            if (offCount % bufferSize === 0) {
+                debug("counts: off=%o trunk=%o trunkWFacets=%o facetStored=%o", offCount, trunkCount, trunkWithFacetCount, writenFacets)
             }
             if (off.quantity != null && off._id.length === 13) {
                 const quantity = toBqt(off.quantity)
@@ -44,18 +45,19 @@ export const offImport = async ([offDb, bfDb]) => {
                     trunkFacets = facetBuffer.length
                     toFacet(quantity, _id, off, facetEntries, entryKeys, facetBuffer)
                     trunkFacets = facetBuffer.length - trunkFacets
+                    debug(trunkFacets)
                     if (trunkFacets > 0) {
                         trunkWithFacetCount++
                         //debug("off._id=%o trunkFacets=%o bufferSize=%o", off._id, trunkFacets, facetBuffer.length)
-                        if (facetBuffer.length >= 1000) {
+                        if (facetBuffer.length >= bufferSize) {
                             //debug("write %o facets", facetBuffer.length)
-                            facetCount += facetBuffer.length
+                            writenFacets += facetBuffer.length
                             //await facets.bulkWrite(facetBuffer, {ordered: false})
                             facetBuffer = []
                         }
 
                         trunkBuffer.push(toTrunk(_id, off, quantity))
-                        if (trunkBuffer.length === 1000) {
+                        if (trunkBuffer.length === bufferSize) {
                             await trunks.bulkWrite(trunkBuffer, {ordered: false})
                             trunkBuffer = []
                         }
@@ -67,15 +69,16 @@ export const offImport = async ([offDb, bfDb]) => {
         .then(async () => {
             debug("FINAL write %o facets", facetBuffer.length)
             if (facetBuffer.length > 0) {
-                facetCount += facetBuffer.length
+                writenFacets += facetBuffer.length
                 //await facets.bulkWrite(facetBuffer, {ordered: false})
                 facetBuffer = []
             }
 
+            debug("FINAL write %o trunks", trunkBuffer.length)
             if (trunkBuffer.length > 0) {
                 await trunks.bulkWrite(trunkBuffer, {ordered: false})
                 trunkBuffer = []
             }
-            debug("FINAL counts: off=%o trunk=%o trunkWFacets=%o facetStored=%o", offCount, trunkCount, trunkWithFacetCount, facetCount)
+            debug("FINAL counts: offs=%o trunks=%o trunkWFacets=%o writenFacets=%o", offCount, trunkCount, trunkWithFacetCount, writenFacets)
         })
 }
