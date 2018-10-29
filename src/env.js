7b7d2a5a038b5a164736bfab3f24c995970a7a8d
slim <smedini@gmail.com>
2018-10-29 10:02:23 +0100

    1.3.2

--- src/env.js
+++ src/env.js
@@ -4,12 +4,17 @@ import {version, name} from './../package.json'
 const ENV = {
     NAME: name,
 
+    DB_HOST_OFF: process.env.DB_HOST_OFF || "localhost",
     DB_NAME_OFF: process.env.DB_NAME_OFF || "off",
-    DB_NAME: process.env.DB_NAME || "BlueForestTreesDB",
+    DB_PORT_OFF: process.env.DB_PORT || 27017,
+    DB_USER_OFF: process.env.DB_USER || "doudou",
+    DB_PWD_OFF: process.env.DB_PWD || "masta",
+
     DB_HOST: process.env.DB_HOST || "localhost",
+    DB_NAME: process.env.DB_NAME || "BlueForestTreesDB",
     DB_PORT: process.env.DB_PORT || 27017,
-    DB_USER: process.env.DB_USER || "doudou",
-    DB_PWD: process.env.DB_PWD || "masta",
+    DB_USER: process.env.DB_USER || "god",
+    DB_PWD: process.env.DB_PWD || "tirlititi",
 
     NODE_ENV: process.env.NODE_ENV || null,
     VERSION: version,
