31fcba51353ba616cd03bcd9bf7eac5d4de9a1a4
slim <smedini@gmail.com>
2018-10-31 12:26:10 +0100

    test workload job

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
-import {offImport} from "./off-import"
 
-const debug = require('debug')('api:off-import')
+console.log("COUCOU JOB!!!")
 
-import mongodb from 'mongodb'
-import ENV from './env'
+// import {offImport} from "./off-import"
+//
+// const debug = require('debug')('api:off-import')
+//
+// import mongodb from 'mongodb'
+// import ENV from './env'
+//
+// const auth = ENV => (ENV.DB_USER && ENV.DB_PWD) ? (ENV.DB_USER + ":" + ENV.DB_PWD + "@") : ""
 
-const auth = ENV => (ENV.DB_USER && ENV.DB_PWD) ? (ENV.DB_USER + ":" + ENV.DB_PWD + "@") : ""
-
-Promise
-    .all([
-        mongodb.MongoClient.connect(`mongodb://${auth(ENV)}${ENV.DB_HOST}:${ENV.DB_PORT}/${ENV.DB_NAME_OFF}?authSource=admin`, {useNewUrlParser: true}).then(client => client.db(ENV.DB_NAME_OFF)),
-        mongodb.MongoClient.connect(`mongodb://${auth(ENV)}${ENV.DB_HOST}:${ENV.DB_PORT}/${ENV.DB_NAME}?authSource=admin`, {useNewUrlParser: true}).then(client => client.db(ENV.DB_NAME))
-    ])
-    .then(offImport)
+// Promise
+//     .all([
+//         mongodb.MongoClient.connect(`mongodb://${auth(ENV)}${ENV.DB_HOST}:${ENV.DB_PORT}/${ENV.DB_NAME_OFF}?authSource=admin`, {useNewUrlParser: true}).then(client => client.db(ENV.DB_NAME_OFF)),
+//         mongodb.MongoClient.connect(`mongodb://${auth(ENV)}${ENV.DB_HOST}:${ENV.DB_PORT}/${ENV.DB_NAME}?authSource=admin`, {useNewUrlParser: true}).then(client => client.db(ENV.DB_NAME))
+//     ])
+//     .then(offImport)
