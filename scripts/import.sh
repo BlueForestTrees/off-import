84c08a5d9f90f797dd36418e2a85558cbfb3719f
slim <smedini@gmail.com>
2018-11-01 18:48:45 +0100

    1.0.2 mongo embedded - mauvaise idée

--- scripts/import.sh
+++ scripts/import.sh
@@ -1,11 +1,30 @@
 #!/usr/bin/env bash
 
-curl https://world.openfoodfacts.org/data/openfoodfacts-mongodbdump.tar.gz --output off.tar.gz #download
-tar -zxvf off.tar.gz #extract
-rm off.tar.gz #clean download
+#download
+printf "\ncurl https://world.openfoodfacts.org/data/openfoodfacts-mongodbdump.tar.gz --output off.tar.gz"
+curl https://world.openfoodfacts.org/data/openfoodfacts-mongodbdump.tar.gz --output off.tar.gz
 
-mongorestore --collection TrunkOff --db off dump/off/ #dump
-rm -rf ./tmp/$OFF_DUMP_NAME #clean extract
-npm run start #import bf
-mongo delete #clean dump
+#extract
+printf "tar -zxvf off.tar.gz"
+tar -zxvf off.tar.gz
+
+#clean download
+printf "rm off.tar.gz"
+rm off.tar.gz
+
+#dump
+printf "mongorestore --collection TrunkOff --db off dump/off/products.bson"
+mongorestore --collection TrunkOff --db off dump/off/products.bson
+
+#clean extract
+printf "rm -rf ./dump"
+rm -rf ./dump
+
+#import bf
+printf "npm run start"
+npm run start
+
+# clean dump
+printf "mongo delete"
+#mongo delete
 
