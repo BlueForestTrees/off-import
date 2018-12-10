a59d714e20e0bec625d4e97b7f1c147236ec873c
slim <smedini@gmail.com>
2018-12-10 17:57:55 +0100

    1.0.9 - JWT_SECRET

--- off-import.sh
+++ off-import.sh
@@ -1,23 +1,24 @@
+
 #download
-printf "wget -O off.tar.gz https://world.openfoodfacts.org/data/openfoodfacts-mongodbdump.tar.gz"
+echo "wget -O off.tar.gz https://world.openfoodfacts.org/data/openfoodfacts-mongodbdump.tar.gz"
 wget -O off.tar.gz https://world.openfoodfacts.org/data/openfoodfacts-mongodbdump.tar.gz
 
 #extract
-printf "tar -zxvf off.tar.gz"
+echo "tar -zxvf off.tar.gz"
 tar -zxvf off.tar.gz
 
 #clean download
-printf "rm off.tar.gz"
+echo "rm off.tar.gz"
 rm off.tar.gz
 
 #dump
-printf "mongorestore --collection TrunkOff --db off dump/off/products.bson"
+echo "mongorestore --collection TrunkOff --db off dump/off/products.bson"
 mongorestore --collection TrunkOff --db off dump/off/products.bson
 
 #clean extract
-printf "rm -rf ./dump"
+echo "rm -rf ./dump"
 rm -rf ./dump
 
 #import bf
-printf "npm run start"
+echo "npm run start"
 npm run start
