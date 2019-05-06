f52050817ae84ad2e05f60ef71b28d3afbb4592f
slim <smedini@gmail.com>
2019-05-06 11:32:06 +0200

    wip

--- src/env.js
+++ src/env.js
@@ -5,15 +5,10 @@ import fs from 'fs'
 const ENV = {
     NAME: name,
 
-    IMPORT_FILTER: process.env.IMPORT_FILTER || '{}',
+    PRODUCT_PATH: "",
+    NO_CAT: (process.env.NO_CAT && Boolean(process.env.NO_CAT)) || false,
 
     DB: {},
-    DB_CONNECTION_STRING_OFF: process.env.DB_CONNECTION_STRING_OFF,
-    DB_HOST_OFF: process.env.DB_HOST_OFF || "localhost",
-    DB_NAME_OFF: process.env.DB_NAME_OFF || "off",
-    DB_PORT_OFF: process.env.DB_PORT_OFF || 27017,
-    DB_USER_OFF: process.env.DB_USER_OFF || "",
-    DB_PWD_OFF: process.env.DB_PWD_OFF || "",
 
     DB_CONNECTION_STRING: process.env.DB_CONNECTION_STRING,
     DB_HOST: process.env.DB_HOST || "localhost",
@@ -31,8 +26,6 @@ const ENV = {
     RB_PATH: process.env.RB_PATH || "mq.json",
 
     LOG_EVERY: process.env.LOG_EVERY && parseInt(process.env.LOG_EVERY) || 200,
-    SKIP: process.env.SKIP && parseInt(process.env.SKIP) || 0,
-    LIMIT: process.env.LIMIT && parseInt(process.env.LIMIT) || 100000
 }
 
 ENV.RB = JSON.parse(fs.readFileSync(ENV.RB_PATH, 'utf8'))
