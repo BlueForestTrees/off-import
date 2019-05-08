24e01b1825117eef29f1b81d075b6dbd8a7694a7
slim <smedini@gmail.com>
2019-05-08 14:59:16 +0200

    1.2.3 - improve build

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
+FROM node:alpine AS build
+
+COPY . .
+RUN yarn install && yarn build
+
 FROM node:alpine
-
-COPY package.json .
-COPY src ./src
-
-RUN npm install
-RUN npm run build
+COPY --from=build /app/dist /
---
+++ src/off-import.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#download
+echo "wget -O off.tar.gz https://world.openfoodfacts.org/data/openfoodfacts-mongodbdump.tar.gz"
+wget -O off.tar.gz https://world.openfoodfacts.org/data/openfoodfacts-mongodbdump.tar.gz
+
+#extract
+echo "tar -zxvf off.tar.gz"
+tar -zxvf off.tar.gz
+
+#run
+node /dist/index.js
